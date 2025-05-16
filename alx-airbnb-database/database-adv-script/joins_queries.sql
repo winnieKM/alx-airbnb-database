55c0b142da1d7e2597fe4434221df342c1b2a6c1
winnieKM <winniemorris254@example.com>
2025-05-16 12:39:53 +0300

    Fix FULL OUTER JOIN implementation in Task 0

--- alx-airbnb-database/database-adv-script/joins_queries.sql
+++ alx-airbnb-database/database-adv-script/joins_queries.sql
@@ -8,16 +8,6 @@ SELECT p.id AS property_id, p.name, r.id AS review_id, r.comment
 FROM properties p
 LEFT JOIN reviews r ON p.id = r.property_id;
 
--- FULL OUTER JOIN workaround using UNION of LEFT and RIGHT JOINs
-SELECT u.id AS user_id, b.id AS booking_id, u.name, b.start_date
-FROM users u
-LEFT JOIN bookings b ON u.id = b.user_id
-
-UNION
-
-SELECT u.id AS user_id, b.id AS booking_id, u.name, b.start_date
-FROM users u
-RIGHT JOIN bookings b ON u.id = b.user_id;
 -- FULL OUTER JOIN workaround using UNION
 SELECT u.id AS user_id, b.id AS booking_id, u.name, b.start_date
 FROM users u
@@ -28,5 +18,6 @@ UNION
 SELECT u.id AS user_id, b.id AS booking_id, u.name, b.start_date
 FROM users u
 RIGHT JOIN bookings b ON u.id = b.user_id;
+
 
 
