d1937bfa06275b3814af50acf4997a8e2d626a9a
winnieKM <winniemorris254@example.com>
2025-05-16 14:11:21 +0300

    Add EXPLAIN ANALYZE for query performance check

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -6,3 +6,13 @@ CREATE INDEX idx_reviews_property_id ON reviews(property_id);
 
 -- Create index on start_date in bookings to speed up date-range queries
 CREATE INDEX idx_bookings_start_date ON bookings(start_date);
+-- Explain query performance before index
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 1;
+
+-- Create an index to optimize queries filtering by user_id
+CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+
+-- Explain query performance after index
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 1;
