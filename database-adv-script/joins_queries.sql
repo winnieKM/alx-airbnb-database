628a2012f78a2fc63665a630b5e33339566f4eb0
winnieKM <winniemorris254@example.com>
2025-05-16 13:54:17 +0300

    Fix LEFT JOIN query for task 0

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,10 +3,11 @@ SELECT booking.*, user.*
 FROM booking
 INNER JOIN user ON booking.user_id = user.id;
 
--- LEFT JOIN example
+-- LEFT JOIN: All properties and their reviews (even if there are no reviews)
 SELECT property.*, review.*
 FROM property
 LEFT JOIN review ON property.id = review.property_id;
+
 
 -- FULL OUTER JOIN example using UNION for MySQL
 SELECT user.*, booking.*
