ae4f88c1ab7d75b9b80567a5a231865ebb69d4e3
winnieKM <winniemorris254@example.com>
2025-05-16 12:38:37 +0300

    Fix FULL OUTER JOIN implementation in Task 0

--- alx-airbnb-database/database-adv-script/joins_queries.sql
+++ alx-airbnb-database/database-adv-script/joins_queries.sql
@@ -1,14 +1,14 @@
--- INNER JOIN: Retrieve all bookings and the respective users
+-- INNER JOIN: Retrieve all bookings and their users
 SELECT b.id AS booking_id, u.id AS user_id, u.name, b.start_date, b.end_date
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
--- LEFT JOIN: Retrieve all properties and their reviews, including those with no reviews
+-- LEFT JOIN: Retrieve all properties and their reviews (including properties with no reviews)
 SELECT p.id AS property_id, p.name, r.id AS review_id, r.comment
 FROM properties p
 LEFT JOIN reviews r ON p.id = r.property_id;
 
--- FULL OUTER JOIN workaround using UNION (because MySQL doesn't support FULL OUTER JOIN)
+-- FULL OUTER JOIN workaround using UNION of LEFT and RIGHT JOINs
 SELECT u.id AS user_id, b.id AS booking_id, u.name, b.start_date
 FROM users u
 LEFT JOIN bookings b ON u.id = b.user_id
@@ -18,4 +18,15 @@ UNION
 SELECT u.id AS user_id, b.id AS booking_id, u.name, b.start_date
 FROM users u
 RIGHT JOIN bookings b ON u.id = b.user_id;
+-- FULL OUTER JOIN workaround using UNION
+SELECT u.id AS user_id, b.id AS booking_id, u.name, b.start_date
+FROM users u
+LEFT JOIN bookings b ON u.id = b.user_id
+
+UNION
+
+SELECT u.id AS user_id, b.id AS booking_id, u.name, b.start_date
+FROM users u
+RIGHT JOIN bookings b ON u.id = b.user_id;
+
 
